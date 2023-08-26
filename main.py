23cba2ebe25ccaf04b1023422e002d266e2ab8ae
AndrewRiegler <andrewriegler@yahoo.com>
2023-08-26 12:17:15 -0700

    Done

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     ##################################################
     """
 
+    celcius = int(input('Enter the number of the temperature in Celsius'))
+    fahrenheit = 9/5 * celcius + 32
+
+    print (f'Farenheit: {fahrenheit:.2f}')
+    
     """
     ########################################
     # Do not delete the return statement
